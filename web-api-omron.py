6b3b80afbd2f3c2c3a0cfd0def0af10c87fc6ab3
adityaekasantoso <adityaekasantoso12@gmail.com>
2026-04-19 11:58:22 +0700

    Update server port in web API to 6100

--- web-api-omron.py
+++ web-api-omron.py
@@ -108,4 +108,4 @@ def get_latest_device():
     return jsonify({})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9000, debug=True)
+    app.run(host="0.0.0.0", port=6100, debug=True)
